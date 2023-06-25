86a7bc8270896be6e09f2c329aba4b0cec3ab301
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-06-24 20:44:18 -0600

    Create deoendencies.sh

---
+++ scripts/deoendencies.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#intended for a new bare system or vm/container
+#installs most common cosmos SDK dependencies, some tools, sets PATH/GOPATH etc in bashrc
+apt-get update > /dev/null && apt-get upgrade -y > /dev/null
+apt-get install nano make build-essential gcc git jq chrony tar curl lz4 wget -y > /dev/null
+wget -q https://go.dev/dl/go1.20.linux-amd64.tar.gz > /dev/null
+rm -rf /usr/local/go > /dev/null && tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz > /dev/null
+rm go1.20.linux-amd64.tar.gz
+if [ "$GOROOT"  = '' ]
+then
+echo -e "export GOROOT=/usr/local/go\nexport GOPATH=$HOME/go\nexport GO111MODULE=on\nexport PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> .bashrc
+source .bashrc
+fi
