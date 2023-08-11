7bea03240bd3890f70ae6d719bec71a54780c2d0
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-08-11 09:22:37 -0600

    Update dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
-#intended for a new bare system or vm/container
+#intended for a new bare debian system or vm/container
 #installs most common cosmos SDK dependencies, some tools, sets PATH/GOPATH etc in bashrc
 apt-get update > /dev/null && apt-get upgrade -y > /dev/null
 apt-get install nano make build-essential gcc git jq chrony tar curl lz4 wget -y > /dev/null
+if [ "$(which go)"  = '' ]
+then
 wget -q https://go.dev/dl/go1.20.linux-amd64.tar.gz > /dev/null
 rm -rf /usr/local/go > /dev/null && tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz > /dev/null
 rm go1.20.linux-amd64.tar.gz
+fi
 if [ "$GOROOT"  = '' ]
 then
 echo -e "export GOROOT=/usr/local/go\nexport GOPATH=$HOME/go\nexport GO111MODULE=on\nexport PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> .bashrc
