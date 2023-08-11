58d2d3386afbcafccfca15bc8cd48b036f68c437
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-08-11 09:29:42 -0600

    Update dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,16 +1,24 @@
-#! /bin/bash
-#intended for a new bare debian system or vm/container
-#installs most common cosmos SDK dependencies, some tools, sets PATH/GOPATH etc in bashrc
+#!/bin/bash
+
+#intended for new debian system/vm/container
+#installs common cosmos SDK dependencies, QOL tools, sets PATH/GOPATH+
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Update and upgrade the system
 apt-get update > /dev/null && apt-get upgrade -y > /dev/null
+
+# Install required packages
 apt-get install nano make build-essential gcc git jq chrony tar curl lz4 wget -y > /dev/null
-if [ "$(which go)"  = '' ]
-then
-wget -q https://go.dev/dl/go1.20.linux-amd64.tar.gz > /dev/null
-rm -rf /usr/local/go > /dev/null && tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz > /dev/null
-rm go1.20.linux-amd64.tar.gz
-fi
-if [ "$GOROOT"  = '' ]
-then
-echo -e "export GOROOT=/usr/local/go\nexport GOPATH=$HOME/go\nexport GO111MODULE=on\nexport PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> .bashrc
-source .bashrc
+
+# Check if Go is installed
+if ! command -v go &> /dev/null; then
+    # Download and install Go
+    wget -q https://go.dev/dl/go1.20.linux-amd64.tar.gz
+    tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz
+    rm go1.20.linux-amd64.tar.gz
+    
+    # Add environment variables to ~/.bashrc
+    echo -e "export GOROOT=/usr/local/go\nexport GOPATH=\$HOME/go\nexport GO111MODULE=on\nexport PATH=\$PATH:/usr/local/go/bin:\$HOME/go/bin" >> ~/.bashrc
+    source ~/.bashrc
 fi
