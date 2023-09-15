91518f15b1174d4ea72ebeab5db03934701c78a9
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-09-15 14:47:33 -0600

    Update print-val-list.sh

--- scripts/print-val-list.sh
+++ scripts/print-val-list.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #prints a list of all validators on chain by moniker:address 
-
+#requires 2 args -- output path/filename and chain daemon name [junod, osmosisd etc]
 output_file=$1
+DAEMOND=$2
 temp_file="temp.json"
 
-daemond q staking validators -o json | jq '[.validators[] | {moniker: .description.moniker, pubkey: .consensus_pubkey.key}]' | \
+$DAEMOND q staking validators -o json | jq '[.validators[] | {moniker: .description.moniker, pubkey: .consensus_pubkey.key}]' | \
 jq -c '.[]' | while read -r line; do
     moniker=$(echo "$line" | jq -r '.moniker')
     pubkey=$(echo "$line" | jq -r '.pubkey')
     
-    pubkey_hex=$(temporald debug pubkey-raw "$pubkey" -t ed25519 | grep "Address:" | sed 's/Address: //')
+    pubkey_hex=$($DAEMOND debug pubkey-raw "$pubkey" -t ed25519 | grep "Address:" | sed 's/Address: //')
     
     echo "$line" | jq --arg pubkey_hex "$pubkey_hex" \
         '. + {address: $pubkey_hex}' >> "$temp_file"
