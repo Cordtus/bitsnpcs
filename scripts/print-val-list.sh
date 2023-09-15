53dc21322a42834ee05c6d847beadce0a81f9900
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-09-15 14:43:00 -0600

    Create print-val-list.sh

---
+++ scripts/print-val-list.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#prints a list of all validators on chain by moniker:address 
+
+output_file=$1
+temp_file="temp.json"
+
+daemond q staking validators -o json | jq '[.validators[] | {moniker: .description.moniker, pubkey: .consensus_pubkey.key}]' | \
+jq -c '.[]' | while read -r line; do
+    moniker=$(echo "$line" | jq -r '.moniker')
+    pubkey=$(echo "$line" | jq -r '.pubkey')
+    
+    pubkey_hex=$(temporald debug pubkey-raw "$pubkey" -t ed25519 | grep "Address:" | sed 's/Address: //')
+    
+    echo "$line" | jq --arg pubkey_hex "$pubkey_hex" \
+        '. + {address: $pubkey_hex}' >> "$temp_file"
+done
+
+echo "[" > "$output_file"
+cat "$temp_file" >> "$output_file"
+echo "]" >> "$output_file"
+
+rm "$temp_file"
