165fe98e024a1cdceb2b4ed993b5f82c4b6b4470
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-09-24 12:20:12 -0600

    Update getblksize.sh

--- scripts/getblksize.sh
+++ scripts/getblksize.sh
@@ -1,74 +1,79 @@
 #!/bin/bash
 
 #show average block size for tendermint chain [250 most blocks default]
-#specify range of blocks by flags '-s', '-e' [potentially extreme hurt to node]
-#getblksize.sh http://rpc.node:26657 -s 128900 -e 133900
+#specify range of blocks by flags '-s', '-e' [potential extreme hurt to node]
+#excludes invalid or rate limited calls from final result
 
-# Initialize variables to keep track of the sum and count
-total_size=0
-count=0
+# Temporary file to hold individual downloaded sizes
+temp_file=$(mktemp)
 
-# Initialize start_height and end_height to empty
-start_height=""
-end_height=""
+# Function to fetch block and write its size to temp_file
+fetch_block() {
+local rpc=$1
+local height=$2
+local response=$(curl -s "$rpc/block?height=$height")
+local size=$(echo -n "$response" | wc -c)
 
-# Parse command-line flags for start_height and end_height
+# Check if the response is in the expected format
+if [[ "$response" == *'{"jsonrpc":"'*'"id":'*'"result":{"block_id":'* ]]; then
+echo $size >> $temp_file
+else
+echo "Skipped block $height due to non-standard format or fetch failure."
+fi
+}
+
+export -f fetch_block
+export temp_file
+
+# Process flags and arguments
 while getopts ":s:e:" opt; do
-  case $opt in
-    s) start_height="$OPTARG"
-    ;;
-    e) end_height="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    exit 1
-    ;;
-  esac
+case $opt in
+s) start_height="$OPTARG";;
+e) end_height="$OPTARG";;
+\?) echo "Invalid option -$OPTARG" >&2; exit 1;;
+esac
 done
 
-# Remove the flags that were parsed by getopts
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 
-# RPC endpoint
 rpc=$1
 
-# Check if RPC endpoint is specified
 if [[ -z "$rpc" ]]; then
-  echo "Usage: $0 [-s start_height] [-e end_height] <rpc_endpoint>"
-  exit 1
+echo "Usage: $0 [-s start_height] [-e end_height] <rpc_endpoint>"
+exit 1
 fi
 
-# Fetch the current block height if end_height is not specified
+# Calculate default heights if not specified
 if [[ -z "$end_height" ]]; then
-  end_height=$(curl -s "$rpc/block" | jq -r '.result.block.header.height')
+end_height=$(curl -s "$rpc/block" | jq -r '.result.block.header.height')
 fi
 
-# Calculate the start_height if not specified
 if [[ -z "$start_height" ]]; then
-  start_height=$((end_height - 250))
+start_height=$((end_height - 250))
 fi
 
-# Check if current_height and start_height were successfully set
-if [[ -z "$end_height" || -z "$start_height" ]]; then
-  echo "Could not determine block heights."
-  exit 1
-fi
-
-echo "Fetching blocks from height $start_height to $end_height."
-
-# Iterate over the determined range of block heights
+# Concurrently fetch block sizes
 for height in $(seq "$start_height" "$end_height"); do
-  # Use curl to fetch the block data and get the size of downloaded data
-  size=$(curl -s "$rpc/block?height=$height" -o /dev/null -s -S -w "%{size_download}")
-
-  # Accumulate the size and increment the count
-  total_size=$((total_size + size))
-  count=$((count + 1))
+fetch_block "$rpc" "$height" &
+if (( $(jobs -p | wc -l) >= 10 )); then
+wait -n
+fi
 done
 
-# Calculate the average size
+# Wait for all jobs to complete
+wait
+
+# Calculate total_size and count
+total_size=$(awk '{s+=$1} END {print s}' $temp_file)
+count=$(wc -l < $temp_file)
+
+# Remove temporary file
+rm -f $temp_file
+
+# Calculate and display average size
 if [ "$count" -gt 0 ]; then
-  average_size=$(echo "scale=2; $total_size / $count" | bc)
-  echo "Average Size: $average_size bytes"
+average_size=$(echo "scale=2; $total_size / $count" | bc)
+echo "Average Size: $average_size bytes"
 else
-  echo "No blocks were fetched. Average size cannot be calculated."
+echo "No blocks were fetched. Average size cannot be calculated."
 fi
