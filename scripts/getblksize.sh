40df5736efb1eea4e190484cd4a65eaf627410f2
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-09-24 13:21:01 -0600

    Update getblksize.sh

--- scripts/getblksize.sh
+++ scripts/getblksize.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#show average block size for tendermint chain [250 most blocks default]
-#specify range of blocks by flags '-s', '-e' [potential extreme hurt to node]
-#excludes invalid or rate limited calls from final result
+# Show average block size for a Tendermint chain (250 most recent blocks by default).
+# Specify the range of blocks by flags '-s', '-e' (potential extreme load on the node).
+# Excludes invalid or rate-limited calls from the final result.
 
 # Temporary file to hold individual downloaded sizes
 temp_file=$(mktemp)
@@ -13,7 +13,6 @@ fetch_block() {
     local response=$(curl -s "$rpc/block?height=$height")
     local size=$(echo -n "$response" | wc -c)
 
-    # Check if the response contains a "block_id" field or a "header" field, as these are likely mandatory for a valid block.
     local has_block_id=$(echo "$response" | jq -e '.result.block_id' > /dev/null 2>&1; echo $?)
     local has_header=$(echo "$response" | jq -e '.result.block.header' > /dev/null 2>&1; echo $?)
 
@@ -27,39 +26,43 @@ fetch_block() {
 export -f fetch_block
 export temp_file
 
-# Process flags and arguments
-while getopts ":s:e:" opt; do
-case $opt in
-s) start_height="$OPTARG";;
-e) end_height="$OPTARG";;
-\?) echo "Invalid option -$OPTARG" >&2; exit 1;;
-esac
-done
-
-shift $((OPTIND - 1))
-
-rpc=$1
-
-if [[ -z "$rpc" ]]; then
-echo "Usage: $0 [-s start_height] [-e end_height] <rpc_endpoint>"
-exit 1
+# Check if the first argument (RPC endpoint) is present
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 <rpc_endpoint> [-s start_height] [-e end_height]"
+    exit 1
 fi
 
-# Calculate default heights if not specified
+# Assign the first argument to the rpc variable and shift arguments
+rpc=$1
+shift
+
+# Process flags
+start_height=""
+end_height=""
+
+while getopts ":s:e:" opt; do
+    case $opt in
+        s) start_height="$OPTARG";;
+        e) end_height="$OPTARG";;
+        \?) echo "Invalid option -$OPTARG" >&2; exit 1;;
+    esac
+done
+
+# Set default heights if not specified
 if [[ -z "$end_height" ]]; then
-end_height=$(curl -s "$rpc/block" | jq -r '.result.block.header.height')
+    end_height=$(curl -s "$rpc/block" | jq -r '.result.block.header.height')
 fi
 
 if [[ -z "$start_height" ]]; then
-start_height=$((end_height - 250))
+    start_height=$((end_height - 250))
 fi
 
 # Concurrently fetch block sizes
 for height in $(seq "$start_height" "$end_height"); do
-fetch_block "$rpc" "$height" &
-if (( $(jobs -p | wc -l) >= 10 )); then
-wait -n
-fi
+    fetch_block "$rpc" "$height" &
+    if (( $(jobs -p | wc -l) >= 10 )); then
+        wait -n
+    fi
 done
 
 # Wait for all jobs to complete
@@ -74,8 +77,8 @@ rm -f $temp_file
 
 # Calculate and display average size
 if [ "$count" -gt 0 ]; then
-average_size=$(echo "scale=2; $total_size / $count" | bc)
-echo "Average Size: $average_size bytes"
+    average_size=$(echo "scale=2; $total_size / $count" | bc)
+    echo "Average Size: $average_size bytes"
 else
-echo "No blocks were fetched. Average size cannot be calculated."
+    echo "No blocks were fetched. Average size cannot be calculated."
 fi
