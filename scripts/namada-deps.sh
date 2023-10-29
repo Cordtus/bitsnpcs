ffd8930adf63c18a47a7555edb504e0ea7a3845e
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-10-29 11:36:37 -0600

    Update namada-deps.sh

--- scripts/namada-deps.sh
+++ scripts/namada-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#intended for new debian system/vm/container
-#installs common cosmos SDK dependencies, QOL tools, sets PATH/GOPATH+
+# Intended for new debian system/vm/container
+# Installs common cosmos SDK dependencies, QOL tools, sets PATH/GOPATH+
 
 set -e
 
@@ -8,15 +8,22 @@ apt-get update > /dev/null && apt-get upgrade -y > /dev/null
 
 apt-get install nano make clang openssl llvm build-essential gcc git jq chrony tar curl lz4 wget -y > /dev/null
 
+# Check if 'go' is installed
 if ! command -v go &> /dev/null; then
-wget -q https://go.dev/dl/go1.20.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz
-rm go1.20.linux-amd64.tar.gz
+    wget -q https://go.dev/dl/go1.20.linux-amd64.tar.gz
+    tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz
+    rm go1.20.linux-amd64.tar.gz
 fi
-if  [ ${GOPATH} = "" ]; then
-echo -e "export GOROOT=/usr/local/go\nexport GOPATH=\$HOME/go\nexport GO111MODULE=on\nexport PATH=\$PATH:/usr/local/go/bin:\$HOME/go/bin" >> ~/.bashrc
+
+# Check if GOPATH is set
+if [ -z "${GOPATH}" ]; then
+    echo -e "export GOROOT=/usr/local/go\nexport GOPATH=\$HOME/go\nexport GO111MODULE=on\nexport PATH=\$PATH:/usr/local/go/bin:\$HOME/go/bin" >> ~/.bashrc
 fi
-if [ $(which rustc) = ""]; then
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Check if rustc is installed
+if [ -z "$(which rustc)" ]; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
+
+# Source the bashrc to apply changes
 source ~/.bashrc
