3c848a40d0f990eddcdf89db09da4d84b6760338
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-10-28 22:18:02 -0600

    Update blksigchk.sh

--- scripts/blksigchk.sh
+++ scripts/blksigchk.sh
@@ -1,7 +1,5 @@
 #~/bin/bash
-#query recent blocks signed/absent/other
-#run on validator rpc, checks back as pruning allows
-#usage - script.sh <url/ip:rpc_port> <blocks_to_check[uint]>
+#usage - script.sh <validator_address:rpc_port> <#_blocks_to_check>
 RPCADDR=$1
 BLOCKS_TO_CHECK=$2
 VALIDATOR_ADDR=$(curl -s $RPCADDR/status | jq -r .result.validator_info.address)
@@ -10,15 +8,15 @@ LAST_BLOCK=$(curl -s $RPCADDR/block | jq -r .result.block.header.height)
 START_BLOCK=$(expr $LAST_BLOCK - $BLOCKS_TO_CHECK)
 
 for BLOCK in $(seq $START_BLOCK $LAST_BLOCK); do
-    RESULT=$(curl -s $RPCADDR/block?height=$BLOCK | jq ".result.block.last_commit.signatures[] | select(.validator_address==\"$VALIDATOR_ADDR\").block_id_flag")
+RESULT=$(curl -s $RPCADDR/block?height=$BLOCK | jq ".result.block.last_commit.signatures[] | select(.validator_address==\"$VALIDATOR_ADDR\").block_id_flag")
 
-    if [ -z "$RESULT" ]; then
-        echo "Block $BLOCK: no signature info"
-    elif [ "$RESULT" == "2" ]; then
-        echo "Block $BLOCK: signed"
-    elif [ "$RESULT" == "1" ]; then
-        echo "Block $BLOCK: not signed"
-    else
-        echo "Block $BLOCK: unknown signature flag: $RESULT"
-    fi
+if [ -z "$RESULT" ]; then
+echo "Block $BLOCK: no signature info"
+elif [ "$RESULT" == "2" ]; then
+echo "Block $BLOCK: signed"
+elif [ "$RESULT" == "1" ]; then
+echo "Block $BLOCK: not signed"
+else
+echo "Block $BLOCK: unknown signature flag: $RESULT"
+fi
 done
