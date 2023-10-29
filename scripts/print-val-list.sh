39b1f936ff6fdf2b6dd08f5d19eb86bb55dcb9c5
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-10-28 22:15:27 -0600

    Update print-val-list.sh

--- scripts/print-val-list.sh
+++ scripts/print-val-list.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-#prints a list of all validators on chain by moniker:address 
-#requires 2 args -- output path/filename and chain daemon name [junod, osmosisd etc]
-output_file=$1
-DAEMOND=$2
-temp_file="temp.json"
 
-$DAEMOND q staking validators -o json | jq '[.validators[] | {moniker: .description.moniker, pubkey: .consensus_pubkey.key}]' | \
-jq -c '.[]' | while read -r line; do
-    moniker=$(echo "$line" | jq -r '.moniker')
-    pubkey=$(echo "$line" | jq -r '.pubkey')
-    
-    pubkey_hex=$($DAEMOND debug pubkey-raw "$pubkey" -t ed25519 | grep "Address:" | sed 's/Address: //')
-    
-    echo "$line" | jq --arg pubkey_hex "$pubkey_hex" \
-        '. + {address: $pubkey_hex}' >> "$temp_file"
+DAEMOND="$1"
+OUTPUT="$2"
+TEMP=$(mktemp)
+declare -a json_array
+
+# Fetch validators and transform to JSON objects
+$DAEMOND q staking validators -o json | jq '[.validators[] | {moniker: .description.moniker, pubkey: .consensus_pubkey.key}]' | jq -c '.[]' | while read -r line; do
+moniker=$(echo "$line" | jq -r '.moniker')
+pubkey=$(echo "$line" | jq -r '.pubkey')
+pubkey_hex=$($DAEMOND debug pubkey-raw $pubkey -t ed25519 | grep "Address:" | sed 's/Address: //')
+json_object=$(echo "$line" | jq --arg pubkey_hex $pubkey_hex '. + {address: $pubkey_hex}')
+printf "%s,\n" "$json_object" >> $TEMP
 done
 
-echo "[" > "$output_file"
-cat "$temp_file" >> "$output_file"
-echo "]" >> "$output_file"
-
-rm "$temp_file"
+# Remove last comma to make it a valid JSON array
+sed '$ s/,$//' $TEMP > $OUTPUT
